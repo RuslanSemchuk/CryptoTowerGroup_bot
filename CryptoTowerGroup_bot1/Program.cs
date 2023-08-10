7505e5718fe15ea8175f3577f6095e0bba088423
RuslanSemchuk <134958046+RuslanSemchuk@users.noreply.github.com>
2023-08-10 20:56:22 +0300

    add bot

---
+++ CryptoTowerGroup_bot1/Program.cs
@@ -0,0 +1,6 @@
+﻿using Telegram.Bot;
+
+var botClient = new TelegramBotClient("{6439952428:AAHgcInkAPuRBxwGpCO7rEGxsc0kOcv_xrk}");
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
